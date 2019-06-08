62f5000bab1144aec12b7630bc81556e57917b3a
MyopicMuppet <47373110+MyopicMuppet@users.noreply.github.com>
2019-06-08 11:48:32 +1000

    ;unch update

--- Assets/Scripts/CharacterCust/CustomisationSet.cs
+++ Assets/Scripts/CharacterCust/CustomisationSet.cs
@@ -51,7 +51,29 @@ public class CustomisationSet : MonoBehaviour
     public InputField characterName;
     public Dropdown cClass;
     public Text className;
-    
+    public Text statPoints;
+    public Text strength;
+    public Text dexterity;
+    public Text constitution;
+    public Text wisdom;
+    public Text intelligence;
+    public Text charisma;
+
+
+    [Header("Point Change Buttons")]
+    public Button strUp;
+    public Button strDn;
+    public Button dexUp;
+    public Button dexDn;
+    public Button conUp;
+    public Button conDn;
+    public Button wisUp;
+    public Button wisDn;
+    public Button intUp;
+    public Button intDn;
+    public Button chrUp;
+    public Button chrDn;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -63,8 +85,15 @@ public class CustomisationSet : MonoBehaviour
         List<string> names = new List<string>(enumNames);
         cClass.AddOptions(names);
 
+       
+
+        
+
+
         statArray = new string[] { "Strength", "Dexterity", "Constitution", "Wisdom", "Intelligence", "Charisma" };
         selectedClass = new string[] { "Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard", };
+
+
 
         #region for loop to pull textures from file
         //for loop looping from 0 to less than the max amount of skin textures we need
@@ -129,6 +158,8 @@ public class CustomisationSet : MonoBehaviour
         SetTexture("Clothes", clothesIndex = 0);
 
         ChooseClass(selectedIndex);
+
+        
 
 
     }
@@ -229,6 +260,59 @@ public class CustomisationSet : MonoBehaviour
         #endregion
     }
     #endregion
+
+    public void Update()
+    {
+        statPoints.text = "Points " + points;
+
+        strength.text = statArray[0] + "" + (stats[0] + tempStats[0]);
+        dexterity.text = statArray[1] + " " + (stats[1] + tempStats[1]);
+        constitution.text = statArray[2] + " " + (stats[2] + tempStats[2]);
+        wisdom.text = statArray[3] + " " + (stats[3] + tempStats[3]);
+        intelligence.text = statArray[4] + " " + (stats[4] + tempStats[4]);
+        charisma.text = statArray[5] + " " + (stats[5] + tempStats[5]);
+
+
+        if (points < 10)
+        {
+            strDn.interactable = true;
+            dexDn.interactable = true;
+            conDn.interactable = true;
+            wisDn.interactable = true;
+            intDn.interactable = true;
+            chrDn.interactable = true;
+        }
+
+        if (points >= 10)
+        {
+            strDn.interactable = false;
+            dexDn.interactable = false;
+            conDn.interactable = false;
+            wisDn.interactable = false;
+            intDn.interactable = false;
+            chrDn.interactable = false;
+        }
+
+        if (points <= 0)
+        {
+            strUp.interactable = false;
+            dexUp.interactable = false;
+            conUp.interactable = false;
+            wisUp.interactable = false;
+            intUp.interactable = false;
+            chrUp.interactable = false;
+
+        }
+        if(points > 0)
+        {
+            strUp.interactable = true;
+            dexUp.interactable = true;
+            conUp.interactable = true;
+            wisUp.interactable = true;
+            intUp.interactable = true;
+            chrUp.interactable = true;
+        }
+    }
 
     void Save()
     {
@@ -463,6 +547,84 @@ public class CustomisationSet : MonoBehaviour
         /*CharacterClass names = (CharacterClass)index;
         className.text = name.ToString();*/
     }
+
+    //I know there is a way to make this just two functions I just couldn't work it out
+    #region Point Change Buttons
+    public void StrengthUpButton()
+    {
+        points--;
+        tempStats[0]++;
+    }
+
+    public void StrengthDownButton()
+    {
+        points++;
+        tempStats[0]--;
+    }
+
+    public void DexUpButton()
+    {
+        points--;
+        tempStats[1]++;
+    }
+
+    public void DexDownButton()
+    {
+        points++;
+        tempStats[1]--;
+    }
+
+    public void ConUpButton()
+    {
+        points--;
+        tempStats[2]++;
+    }
+
+    public void ConDownButton()
+    {
+        points++;
+        tempStats[2]--;
+    }
+
+    public void WisUpButton()
+    {
+        points--;
+        tempStats[3]++;
+    }
+
+    public void WisDownButton()
+    {
+        points++;
+        tempStats[3]--;
+    }
+
+    public void IntUpButton()
+    {
+        points--;
+        tempStats[4]++;
+    }
+
+    public void IntDownButton()
+    {
+        points++;
+        tempStats[4]--;
+    }
+
+    public void ChrUpButton()
+    {
+        points--;
+        tempStats[5]++;
+    }
+
+    public void ChrDownButton()
+    {
+        points++;
+        tempStats[5]--;
+    }
+
+    #endregion
+
+
 }
 
 
